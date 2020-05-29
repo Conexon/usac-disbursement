be8f446e31248c19875bca1c2862fff949b777e3
maplines <feomike@gmail.com>
2020-05-29 08:36:57 -0400

    minor commit

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -7,7 +7,7 @@ const Intro = () => {
         programs from January 2015 through March 2020.
       </p>
       <p className="mt-4">
-        Clicking on the map will result in a table displayed showing the amount of
+        Clicking on the map will result in a table showing the amount of
         funding given to that company to further broadband availability. The orange 
         areas still lack the services that are widely available in urban areas. 
         Unaccounted for funds are typically disbursed to large carriers like AT&T.
